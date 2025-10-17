b50691394823d811d03f3882cf97860ba4183e55
Che Alyssa Zulaik <chealyssa@gmail.com>
2025-10-17 01:03:24 -0500

    finished and tested BoundingBox, StrokeColor, Outline, and Point

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -1,13 +1,13 @@
 package edu.luc.etl.cs313.android.shapes.model;
 
+import android.graphics.Rect;
+
 /**
  * A shape visitor for calculating the bounding box, that is, the smallest
  * rectangle containing the shape. The resulting bounding box is returned as a
  * rectangle at a specific location.
  */
 public class BoundingBox implements Visitor<Location> {
-
-    // TODO entirely your job (except onCircle)
 
     @Override
     public Location onCircle(final Circle c) {
@@ -17,38 +17,54 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
-    }
+        return f.getShape().accept(this);
+    } // done
 
     @Override
     public Location onGroup(final Group g) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
 
-        return null;
-    }
+        for (Shape s : g.getShapes()){
+            final Location box = s.accept(this);
+            final Rectangle rect = (Rectangle) box.getShape();
+            minX = Math.min(minX, box.getX());
+            minY = Math.min(minY, box.getY());
+            maxX = Math.max(maxX, box.getX() + rect.getWidth());
+            maxY = Math.max(maxY, box.getY() + rect.getHeight());
+        }
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
+    } // done
 
     @Override
     public Location onLocation(final Location l) {
-
-        return null;
-    }
+        final Location inner = l.getShape().accept(this);
+        return new Location(
+                l.getX() + inner.getX(),
+                l.getY() + inner.getY(),
+                inner.getShape()
+        );
+    } // done
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
-    }
+        return new Location(0,0,r);
+    } // done
 
     @Override
     public Location onStrokeColor(final StrokeColor c) {
-        return null;
-    }
+        return c.getShape().accept(this);
+    } // done
 
     @Override
     public Location onOutline(final Outline o) {
-        return null;
+        return o.getShape().accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
-    }
+        return onGroup(s);
+    } // done
 }
--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
@@ -6,21 +6,24 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public final class StrokeColor implements Shape {
 
-    // TODO entirely your job
+    private final int color;
+    private final Shape shape;
 
     public StrokeColor(final int color, final Shape shape) {
-    }
+        this.color = color;
+        this.shape = shape;
+    } // done
 
     public int getColor() {
-        return -1;
-    }
+        return color;
+    } // done
 
     public Shape getShape() {
-        return null;
-    }
+        return shape;
+    } // done
 
     @Override
     public <Result> Result accept(Visitor<Result> v) {
-        return null;
-    }
+        return v.onStrokeColor(this);
+    } // done
 }
